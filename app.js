249f013eefb450f3413aa32bdd9f89c83bc316a3
Bharath HS <bharathhs2290@gmail.com>
2016-01-12 19:32:27 +0530

    removed db

--- app.js
+++ app.js
@@ -4,48 +4,48 @@
 var express = require('express');
 
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/iot');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/iot');
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 
-var ioDeviceSchema = new mongoose.Schema({
-    devID:String,
-    temp:Number,
-    swtchState:Boolean,
-    serialno:Number
-});
-
-var iod =mongoose.model('cat',ioDeviceSchema);
-
-iod.create({
-
-  devID:"N8237",
-  temp:58,
-  swtchState:false
-
-},function(err,cat){
- if(err){
-   console.log(err);
- }
-  else{
-   console.log(cat);
- }
-
-});
+//var ioDeviceSchema = new mongoose.Schema({
+//    devID:String,
+//    temp:Number,
+//    swtchState:Boolean,
+//    serialno:Number
+//});
+//
+//var iod =mongoose.model('cat',ioDeviceSchema);
+//
+//iod.create({
+//
+//  devID:"N8237",
+//  temp:58,
+//  swtchState:false
+//
+//},function(err,cat){
+// if(err){
+//   console.log(err);
+// }
+//  else{
+//   console.log(cat);
+// }
+//
+//});
 
 
 
 
 app.get('/',function(req,res){
 
-    iod.find({},function(err,data){
+   // iod.find({},function(err,data){
 
        // console.log(data);
-        res.send(data);
+        res.send("data");
 
     });
 
